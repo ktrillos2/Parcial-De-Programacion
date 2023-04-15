d1afc544d51c7d83a7aaa419368b223f52e2bad3
ktrillos2 <ktrillos2@gmail.com>
2023-04-14 23:00:42 -0500

    Creacion de funcion para agregar cajero

--- Parcial De Programacion.py
+++ Parcial De Programacion.py
@@ -4,25 +4,42 @@ usuarios = [
     ["cajero2@email.com", "cajero2", "cajero",[]],
 ]
 
+def agregarCajero():
+    usuario=input("Ingrese correo")
+    contraseña=input("Ingrese contraseña")
+    nombre=input("Ingrese nombre")
+    while True:
+        documento=input("Ingrese documento")
+        if documento.isDigit():
+            if len(documento) < 8:
+                print("Documento muy corto")
+            elif len(documento)>10:
+                print("Documento muy largo")
+            else:
+                break
+        else:
+            print("Ingresa un numero valido")
+    usuarios.append([usuario,contraseña,"cajero",[nombre,documento,True]])
+    
+    
 
 def iniciarSesion(usuario, contraseña):
     encontrado = False
     for i in usuarios:
         if i[0] == usuario and i[1] == contraseña:
             encontrado = True
-            admin=True
             if i[2] == "admin":
-                while admin:
+                while True:
                         preguntaAdmin = input("1. Cajeros\n2. Ver productos\n3. Ventas\nSelecciona una opción: ")
                         if preguntaAdmin=="1":
                             opcionesCajeros=True
                             while opcionesCajeros:
-                                    preguntaCajero=input("1. Eliminar Cajeros\n2. Agregar Cajero\n3. Regresar al menu anterior\nSelecciona una opción: ")
+                                    preguntaCajero=input("1. Eliminar Cajeros\n2. Agregar Cajero\n3. Modificar Cajero*\n4. Regresar al menu anterior\nSelecciona una opción: ")
                                     if preguntaCajero=="1":
                                         ##Codigo de eliminar cajero
                                         break
                                     elif preguntaCajero=="2":
-                                        ##Codigo para agregar cajero
+                                        agregarCajero()
                                         break
                                     elif preguntaCajero=="3":
                                         opcionesCajeros=False
