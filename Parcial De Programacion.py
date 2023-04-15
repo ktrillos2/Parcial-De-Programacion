dada90c075b12393ed7cbd5566dc4a836f3996e3
ktrillos2 <ktrillos2@gmail.com>
2023-04-14 23:44:01 -0500

    Creacion de funcion para eliminar cajero

--- Parcial De Programacion.py
+++ Parcial De Programacion.py
@@ -1,27 +1,55 @@
 usuarios = [
-    ["admin@email.com", "admin123", "admin",[]],
-    ["cajero1@email.com", "cajero1", "cajero",[]],
-    ["cajero2@email.com", "cajero2", "cajero",[]],
+    ["admin@email.com", "admin123", "admin", []],
+    ["cajero1@email.com", "cajero1", "cajero", ["prueba", "123456789", True]],
+    ["cajero2@email.com", "cajero2", "cajero", ["prueba2", "1234567891", True]],
 ]
 
+
 def agregarCajero():
-    usuario=input("Ingrese correo")
-    contraseña=input("Ingrese contraseña")
-    nombre=input("Ingrese nombre")
+    usuario = input("Ingrese correo: ")
+    contraseña = input("Ingrese contraseña: ")
+    nombre = input("Ingrese nombre: ")
     while True:
-        documento=input("Ingrese documento")
-        if documento.isDigit():
+        documento = input("Ingrese documento: ")
+        if documento.isdigit():
             if len(documento) < 8:
                 print("Documento muy corto")
-            elif len(documento)>10:
+            elif len(documento) > 10:
                 print("Documento muy largo")
             else:
                 break
         else:
-            print("Ingresa un numero valido")
-    usuarios.append([usuario,contraseña,"cajero",[nombre,documento,True]])
-    
-    
+            print("Ingresa un documento valido")
+    usuarios.append([usuario, contraseña, "cajero", [nombre, documento, True]])
+
+
+def eliminarCajero():
+    encontrado = False
+    while True:
+        documento = input("Ingrese documento del cajero a eliminar: ")
+        if documento.isdigit():
+            if len(documento) < 8:
+                print("Documento muy corto")
+            elif len(documento) > 10:
+                print("Documento muy largo")
+            else:
+                break
+        else:
+            print("Ingresa un documento valido")
+
+    for usuario in usuarios:
+        for i in range(len(usuario[3])):
+            if documento == usuario[3][1]:
+                encontrado = True
+                usuario[3][2] = False
+
+    if encontrado == False:
+        print("Documento no encontrado")
+        return False
+    else:
+        print(usuarios)
+        return True
+
 
 def iniciarSesion(usuario, contraseña):
     encontrado = False
@@ -30,31 +58,35 @@ def iniciarSesion(usuario, contraseña):
             encontrado = True
             if i[2] == "admin":
                 while True:
-                        preguntaAdmin = input("1. Cajeros\n2. Ver productos\n3. Ventas\nSelecciona una opción: ")
-                        if preguntaAdmin=="1":
-                            opcionesCajeros=True
-                            while opcionesCajeros:
-                                    preguntaCajero=input("1. Eliminar Cajeros\n2. Agregar Cajero\n3. Modificar Cajero*\n4. Regresar al menu anterior\nSelecciona una opción: ")
-                                    if preguntaCajero=="1":
-                                        ##Codigo de eliminar cajero
-                                        break
-                                    elif preguntaCajero=="2":
-                                        agregarCajero()
-                                        break
-                                    elif preguntaCajero=="3":
-                                        opcionesCajeros=False
-                                        break
-                                    else:
-                                        print("Ingresa un numero valido")
-                            break
-                        elif preguntaAdmin=="2":
-                            ##codigo de productos
-                            break
-                        elif preguntaAdmin=="3":
-                            ##codigo de ventas
-                            break
-                        else:
-                            print("Ingresa un numero valido")
+                    preguntaAdmin = input(
+                        "1. Cajeros\n2. Ver productos\n3. Ventas\nSelecciona una opción: "
+                    )
+                    if preguntaAdmin == "1":
+                        while True:
+                            preguntaCajero = input(
+                                "1. Eliminar Cajeros\n2. Agregar Cajero\n3. Modificar Cajero\n4. Regresar al menu anterior\nSelecciona una opción: "
+                            )
+                            if preguntaCajero == "1":
+                                if eliminarCajero() == True:
+                                    break
+                            elif preguntaCajero == "2":
+                                agregarCajero()
+                                break
+                            elif preguntaCajero == "3":
+                                ##codigo para modificar cajero
+                                break
+                            elif preguntaCajero == "4":
+                                break
+                            else:
+                                print("Ingresa un numero valido")
+                    elif preguntaAdmin == "2":
+                        # codigo de productos
+                        break
+                    elif preguntaAdmin == "3":
+                        # codigo de ventas
+                        break
+                    else:
+                        print("Ingresa un numero valido")
             if i[2] == "cajero":
                 print("Ingresaste como cajero")
             if i[2] == "usuario":
