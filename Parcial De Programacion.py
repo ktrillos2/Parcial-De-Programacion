25b70d4ea76bcbd2ca8e01c2edcd859adb1701aa
ktrillos2 <ktrillos2@gmail.com>
2023-04-24 14:10:08 -0500

    Creacion de funciones para modificar productos

--- Parcial De Programacion.py
+++ Parcial De Programacion.py
@@ -27,19 +27,19 @@ tipos2 = {
 
 # 1 = Vasos
 tamaños_vasos = {
-    "1": [("Sencillo", 5500), False, " -Este producto no se encuentra disponible"],
-    "2": [("Doble", 8400), True, ""],
-    "3": [("Triple", 10800), True, ""],
+    "1": [["Sencillo", 5500], False, " -Este producto no se encuentra disponible"],
+    "2": [["Doble", 8400], True, ""],
+    "3": [["Triple", 10800], True, ""],
 }
 cantidad_bolas_vasos = {"Sencillo": 1, "Doble": 2, "Triple": 3}
 
 # 2 = Conos
 tamaños_conos = {
-    "1": [("Pequeño", 4200), True, ""],
-    "2": [("Mediano", 5500), True, ""],
-    "3": [("Doble", 8400), True, ""],
-    "4": [("Super cono", 10800), True, ""],
-    "5": [("Mega cono", 14000), True, ""],
+    "1": [["Pequeño", 4200], True, ""],
+    "2": [["Mediano", 5500], True, ""],
+    "3": [["Doble", 8400], True, ""],
+    "4": [["Super cono", 10800], True, ""],
+    "5": [["Mega cono", 14000], True, ""],
 }
 
 cantidad_bolas_conos = {
@@ -52,54 +52,54 @@ cantidad_bolas_conos = {
 
 # 3 = Granizados
 tamaños_granizados = {
-    "1": [("Granizado 12 onz", 6400), True, ""],
-    "2": [("Granizado 16 onz", 8400), True, ""],
-    "3": [("Salpicon con helado", 9000), True, ""],
-    "4": [("Salpicon sin helado", 6300), True, ""],
-    "5": [("copa chocolate", 9000), True, ""],
+    "1": [["Granizado 12 onz", 6400], True, ""],
+    "2": [["Granizado 16 onz", 8400], True, ""],
+    "3": [["Salpicon con helado", 9000], True, ""],
+    "4": [["Salpicon sin helado", 6300], True, ""],
+    "5": [["copa chocolate", 9000], True, ""],
 }
 
 # 4 = Copas infantiles
 tamaños_copas_infantiles = {
-    "1": [("Mickey", 6400), True, ""],
-    "2": [("piñata", 10800), True, ""],
-    "3": [("Fiesta", 8800), True, ""],
-    "4": [("chips", 8800), True, ""],
+    "1": [["Mickey", 6400], True, ""],
+    "2": [["piñata", 10800], True, ""],
+    "3": [["Fiesta", 8800], True, ""],
+    "4": [["chips", 8800], True, ""],
 }
 cantidad_bolas_copa_infantil = {
     "Mickey": 1, "piñata": 1, "Fiesta": 2, "chips": 2}
 
 # 5 = Malteadas
 tamaños_malteadas = {
-    "1": [("Malteada 12 onz", 10000), True, ""],
-    "2": [("Malteada 16 onz", 12000), True, ""],
+    "1": [["Malteada 12 onz", 10000], True, ""],
+    "2": [["Malteada 16 onz", 12000], True, ""],
 }
 
 # 6 = Litro de helado
-tamaños_litro_helado = {"1": [("Litro de helado", 27000), True, ""]}
+tamaños_litro_helado = {"1": [["Litro de helado", 27000], True, ""]}
 
 # 7 = Ensalada de frutas
 tamaños_ensalada_frutas = {
-    "1": [("personal", 10800), True, ""],
-    "2": [("para compartir sin helado", 10200), True, ""],
-    "3": [("para compartir con helado", 14500), True, ""],
+    "1": [["personal", 10800], True, ""],
+    "2": [["para compartir sin helado", 10200], True, ""],
+    "3": [["para compartir con helado", 14500], True, ""],
 }
 
 # 8 = Fresas
 tamaños_fresas = {
-    "1": [("Fresas con crema", 11400), True, ""],
-    "2": [("Fresas con helado", 8400), True, ""],
+    "1": [["Fresas con crema", 11400], True, ""],
+    "2": [["Fresas con helado", 8400], True, ""],
 }
 
 # 9 = Copas
 tamaños_copas = {
-    "1": [("Exotica", 10800), True, ""],
-    "2": [("Ron o amaretto", 10800), True, ""],
-    "3": [("Caramelo", 10800), True, ""],
-    "4": [("Tropical", 10800), True, ""],
-    "5": [("Peach melba", 10800), True, ""],
-    "6": [("De la casa", 10800), True, ""],
-    "7": [("Sundae", 10800), False, "a"],
+    "1": [["Exotica", 10800], True, ""],
+    "2": [["Ron o amaretto", 10800], True, ""],
+    "3": [["Caramelo", 10800], True, ""],
+    "4": [["Tropical", 10800], True, ""],
+    "5": [["Peach melba", 10800], True, ""],
+    "6": [["De la casa", 10800], True, ""],
+    "7": [["Sundae", 10800], False, "a"],
 }
 cantidad_bolas = {
     "Exotica": 3,
@@ -112,17 +112,17 @@ cantidad_bolas = {
 }
 
 # 10 = Banana split
-tamaños_banana_split = {"1": [("Banana split", 13200), True, ""]}
+tamaños_banana_split = {"1": [["Banana split", 13200], True, ""]}
 
 # 11 = Brownie con helado
-tamaños_brownie = {"1": [("Brownie con helado", 13200), True, ""]}
+tamaños_brownie = {"1": [["Brownie con helado", 13200], True, ""]}
 
 # 12 SubCategorias Nuevas
 
 subcategorias_nuevas = {1: [
     "galletas", True, "esto no se encuentra disponible ",{
-    "1": [("Exotica", 10800), False, " No se encuentra disponible"],
-    "2": [("Ron o amaretto", 10800), True, ""],
+    "1": [["Exotica", 10800], False, " No se encuentra disponible"],
+    "2": [["Ron o amaretto", 10800], True, ""],
 }], 2: ["sancocho", True, "",{}]}
 
 # ----------------------------------------------------------------Fin de productos----------------------------------------------------------------
@@ -425,29 +425,98 @@ def modificarCajero():
             print("Ingresa una opción valida")
 
 def modificarProducto(productos):
-    nombre_producto = input("Ingresa el nombre del producto que deseas modificar: ")
-    encontrado = False
-    for clave in productos.keys():
-        if nombre_producto in productos[clave][0][0].lower():
-            encontrado = True
-            nombre_producto = clave
-            break
-    if encontrado:
-        print(f"Actualmente el producto '{productos[nombre_producto][0][0]}' tiene el siguiente precio:")
-        print(f"Precio: {productos[nombre_producto][0][1]}")
-        opcion = input("¿Qué deseas modificar? (1) Precio o (2) eliminar el producto: ")
-        if opcion == "1":
-            nuevo_precio = input("Ingresa el nuevo precio del producto: ")
-            productos[nombre_producto][1] = nuevo_precio
-            print(f"El precio del producto '{productos[nombre_producto][0][0]}' ha sido actualizado a {nuevo_precio}.")
-        elif opcion == "2":
-            nueva_descripcion = input("Ingresa la nueva descripción del producto: ")
-            productos[nombre_producto][2] = nueva_descripcion
-            print(f"La descripción del producto '{nombre_producto}' ha sido actualizada a '{nueva_descripcion}'.")
+    while True:
+        print("Opciones de productos disponibles:")
+        for i, producto in enumerate(productos.values()):
+            print(f"{i+1}. {producto[0][0]} - ${producto[0][1]} {producto[2]}")
+        print(f"{len(productos) + 1}. Volver al menú anterior")
+
+        seleccion = input("Ingrese el número correspondiente al producto que desea modificar: ")
+
+        if seleccion.isdigit():
+            seleccion = int(seleccion)
+            if seleccion >= 1 and seleccion <= len(productos):
+                nombre_producto = list(productos.keys())[seleccion-1]
+                print(f"Actualmente el producto '{productos[nombre_producto][0][0]}' tiene el siguiente precio:")
+                print(f"Precio: {productos[nombre_producto][0][1]}")
+                while True:
+                    opcion = input("1. Precio\n2. Eliminar Producto\n3. Volver al menu anterior\n¿Qué deseas modificar?: ")
+                    if opcion == "1":
+                        while True:
+                            nuevo_precio = input("Ingresa el nuevo precio del producto: ")
+                            if nuevo_precio.isdigit():
+                                productos[nombre_producto][0][1] = int(nuevo_precio)
+                                print(f"El precio del producto '{productos[nombre_producto][0][0]}' ha sido actualizado a {nuevo_precio}.")
+                                break
+                            else:
+                                print("Error: el precio debe ser un número entero.")
+                        break
+                    elif opcion == "2":
+                        productos[nombre_producto][1] = False
+                        productos[nombre_producto][2] = "- No se encuentra habilitado"
+                        print(f"el producto '{productos[nombre_producto][0][0]}' ha sido eliminado")
+                        break
+                    elif opcion=="3":
+                        break
+                    else:
+                        print("Opción inválida. Por favor, ingresa 1 o 2.")
+            elif seleccion == len(productos) + 1:
+                break
+            else:
+                print(f"El número de producto '{seleccion}' no es válido. Por favor, ingresa un número válido.")
         else:
-            print("Opción inválida. Por favor, ingresa 1 o 2.")
+            print(f"La opción ingresada '{seleccion}' no es válida. Por favor, ingresa una opción válida.")
+
+
+
+
+
+def modificarProductosSubcategoria(productos):
+    if len(productos) == 0:
+        print("Esta subcategoría no tiene productos.")
     else:
-        print(f"No se encontró el producto '{nombre_producto}' en la lista de productos.")
+        opcionesProductos = ""
+        for i, producto in enumerate(productos.items()):
+            nombreProducto = producto[1][0]
+            disponibleProducto = "disponible" if producto[1][1] else "no disponible"
+            opcionesProductos += f"{i+1}. {nombreProducto[0].capitalize()} - ({disponibleProducto}) - ${producto[1][0][1]}\n"
+        opcionesProductos += f"{len(productos)+1}. Volver al menú anterior\n"
+
+        while True:
+            print(opcionesProductos)
+            preguntaProducto = input("¿Qué producto deseas modificar? Ingresa el número correspondiente: ")
+            if preguntaProducto.isdigit() and int(preguntaProducto) > 0 and int(preguntaProducto) <= len(productos):
+                productoSeleccionado = list(productos.items())[int(preguntaProducto)-1]
+                opcionesModificacion = "1. Cambiar precio\n2. Eliminar producto\n3. Volver al menú anterior\n"
+                print(f"Seleccionaste el producto {productoSeleccionado[1][0][0].capitalize()} ({'disponible' if productoSeleccionado[1][1] else 'no disponible'})")
+
+                while True:
+                    preguntaModificacion = input(f"{opcionesModificacion}¿Qué deseas modificar? Ingresa el número correspondiente: ")
+                    if preguntaModificacion == "1":
+                        nuevoPrecio = input("Ingresa el nuevo precio del producto: ")
+                        while not nuevoPrecio.isdigit():
+                            nuevoPrecio = input("Valor inválido. Ingresa un número válido: ")
+                        productoSeleccionado[1][0][1] = int(nuevoPrecio)
+                        print(f"El precio del producto {productoSeleccionado[1][0][0].capitalize()} se ha actualizado a {nuevoPrecio}.")
+                        break
+                    elif preguntaModificacion == "2":
+                        productoSeleccionado[1][1] = False
+                        productoSeleccionado[1][2] = "- No se encuentra habilitado"
+                        print(f"El producto {productoSeleccionado[1][0][0].capitalize()} ha sido eliminado.")
+                        break
+                    elif preguntaModificacion == "3":
+                        break
+                    else:
+                        print("Opción inválida. Ingresa un número correspondiente a una opción válida.")
+            elif preguntaProducto == str(len(productos)+1):
+                break
+            else:
+                print("Opción inválida. Ingresa un número correspondiente a una opción válida.")
+
+
+
+
+
 
 while True:
     usuario = input("Ingrese usuario: ").strip()
@@ -697,70 +766,48 @@ while True:
                                             productos = {}
                                             if preguntaCategoria == "1":
                                                 productos = tamaños_vasos
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                
                                                 modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "2":
                                                 productos = tamaños_conos
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "3":
                                                 productos = tamaños_granizados
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)        
                                                 break
                                             elif preguntaCategoria == "4":
                                                 productos = tamaños_copas_infantiles
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "5":
                                                 productos = tamaños_malteadas
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "6":
                                                 productos = tamaños_litro_helado
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "7":
                                                 productos = tamaños_ensalada_frutas
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "8":
                                                 productos = tamaños_fresas
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "9":
                                                 productos = tamaños_copas
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "10":
                                                 productos = tamaños_banana_split
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "11":
                                                 productos = tamaños_brownie
-                                                for i in productos:
-                                                    print(
-                                                        productos[i][0], productos[i][2])
+                                                modificarProducto(productos)
                                                 break
                                             elif preguntaCategoria == "12":
                                                 if len(subcategorias_nuevas) == 0:
@@ -790,11 +837,7 @@ while True:
                                                         if len(subcategorias_nuevas[int(preguntaSubCategoria)][3])==0:
                                                             print("Esta subcategoria no tiene productos")
                                                         else:
-                                                            if subcategorias_nuevas[int(preguntaSubCategoria)][1]==True:
-                                                                for i in subcategorias_nuevas[int(preguntaSubCategoria)][3]:
-                                                                    print(subcategorias_nuevas[int(preguntaSubCategoria)][3][i][0]," ",subcategorias_nuevas[int(preguntaSubCategoria)][3][i][2])
-                                                            else:
-                                                                print("Esta subcategoría se encuentra deshabilitada")
+                                                            modificarProductosSubcategoria(subcategorias_nuevas[int(preguntaSubCategoria)][3])
                                                     else:
                                                         print(
                                                             "Opción inválida. Por favor, ingresa un número correspondiente a una opción válida."
@@ -829,9 +872,6 @@ while True:
             )
             if pregunta == "si":
                 # Aqui va codigo para agregar a una persona
-                break
-            elif pregunta == "no":
-                print("Gracias por usar nuestro programa")
                 break
             else:
                 print("Ingresa una opción valida")
